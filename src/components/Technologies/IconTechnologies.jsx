c88cec582e9dcbc83899d3081a7f44fb7aba0c1b
fermin234 <fermin234@hotmail.com>
2023-07-08 18:38:07 -0300

    added IconTechnologie

---
+++ src/components/Technologies/IconTechnologies.jsx
@@ -0,0 +1,198 @@
+import mailJS from '../../assets/Icons/mailJS.png'
+import nodeMailer from '../../assets/Icons/nodemailer.png'
+import chakra from '../../assets/Icons/chakra.png'
+
+export function Javascript() {
+  return (
+    <img
+      alt="javascript"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185231737-1ba31713-13f0-48a3-b352-5c2bef06285c.svg"
+    />
+  )
+}
+
+export function React() {
+  return (
+    <img
+      alt="react"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185236678-4d3a20e6-4ac7-4e61-a2bd-66c98f576ecf.svg"
+    />
+  )
+}
+
+export function Express() {
+  return (
+    <img
+      alt="express"
+      width="30px"
+      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+    />
+  )
+}
+
+export function Nodejs() {
+  return (
+    <img
+      alt="nodejs"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185236647-b3712d46-4a0b-4cf8-b761-d53ca8c26fe2.svg"
+    />
+  )
+}
+
+export function Redux() {
+  return (
+    <img
+      alt="redux"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185237182-d52b1291-83ba-4b5f-a3ef-e0a26277f8ff.svg"
+    />
+  )
+}
+
+export function Sequelize() {
+  return (
+    <img
+      alt="sequelize"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185237348-35a8cbda-a32b-4a64-a9e0-07e466d0a337.svg"
+    />
+  )
+}
+
+export function PostgreSQL() {
+  return (
+    <img
+      alt="postgreSQL"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185237377-f550d006-af86-44c7-a3ac-5fdeafd61e90.svg"
+    />
+  )
+}
+
+export function Typescript() {
+  return (
+    <img
+      alt="typescript"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185237391-95fb91e3-66b7-486f-b0fb-a9786d2e8eb3.svg"
+    />
+  )
+}
+export function Git() {
+  return (
+    <img
+      alt="git"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185237411-06551dea-cd5f-4dd5-9be4-994eeea573bc.svg"
+    />
+  )
+}
+
+export function HTML() {
+  return (
+    <img
+      alt="html"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185236790-0faaaeed-e7ca-4d75-80c9-8b6e0aab1fff.svg"
+    />
+  )
+}
+
+export function CSS() {
+  return (
+    <img
+      alt="css"
+      width="30px"
+      src="https://user-images.githubusercontent.com/102181381/185237561-15f189ed-ed0c-4282-a5c8-41e5b07c3f46.svg"
+    />
+  )
+}
+
+export function Chartjs() {
+  return (
+    <img
+      alt="chartjs"
+      width="40px"
+      src="https://www.chartjs.org/media/logo-title.svg"
+    />
+  )
+}
+
+export function Tailwind() {
+  return (
+    <img
+      alt="tailwind"
+      width="30px"
+      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+    />
+  )
+}
+
+export function Nextjs13() {
+  return (
+    <img
+      alt="nextJS13"
+      width="30px"
+      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+    />
+  )
+}
+
+export function GitHub() {
+  return (
+    <img
+      alt="GitHub"
+      width="30px"
+      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+    />
+  )
+}
+
+export function MailJS() {
+  return (
+    <img
+      alt="MailJS"
+      width="30px"
+      src={mailJS}
+    />
+  )
+}
+
+export function NodeMailer() {
+  return (
+    <img
+      alt="NodeMailer"
+      width="30px"
+      src={nodeMailer}
+    />
+  )
+}
+
+export function Trello() {
+  return (
+    <img
+      alt="Trello"
+      width="30px"
+      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg"
+    />
+  )
+}
+
+export function Chakra() {
+  return (
+    <img
+      alt="Chakra"
+      width="30px"
+      src={chakra}
+    />
+  )
+}
+
+
+
+
+
+
