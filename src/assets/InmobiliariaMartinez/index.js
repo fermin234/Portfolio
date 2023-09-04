cb7b8b105ef64606146e6fb1a0d4d13e3b4527ae
fermin234 <fermin234@hotmail.com>
2023-09-04 18:15:44 -0300

    added assets inmobiliaria

---
+++ src/assets/InmobiliariaMartinez/index.js
@@ -0,0 +1,18 @@
+import a from "./1.webp";
+import b from "./2.webp";
+import c from "./3.webp";
+import d from "./4.webp";
+import e from "./5.webp";
+import f from "./6.webp";
+import g from "./7.webp";
+import h from "./8.webp";
+import i from "./9.webp";
+import j from "./10.webp";
+import k from "./11.webp";
+import l from "./12.webp";
+import m from "./13.webp";
+import n from "./14.webp";
+
+const imagesGameInformer = [a, b, c, d, e, f, g, h, i, j, k, l, m, n];
+
+export default imagesGameInformer;
