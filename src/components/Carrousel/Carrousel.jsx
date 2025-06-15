7a9207007f1507e7dfa0685b02932c43df7ada1b
Fermin Solaberrieta <fermin.solaberrieta@biggertech.co>
2025-06-15 22:48:11 +0000

    Carousel Component

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Carousel = ({ images }) => {
-  const { pathname } = useHistory().location
+  const { pathname } = useHistory().location;
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const nextImage = () => {
@@ -11,7 +11,9 @@ const Carousel = ({ images }) => {
   };
 
   const previousImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
   };
 
   const selectImage = (index) => {
@@ -19,25 +21,35 @@ const Carousel = ({ images }) => {
   };
 
   return (
-    <div className={`relative flex justify-center ${pathname.includes("proyectos") ? "" : "max-h-[532px]"} overflow-hidden`}>
+    <div
+      className={`relative flex justify-center ${
+        pathname.includes("projects") ? "" : "max-h-[532px]"
+      } overflow-hidden`}
+    >
       <button
         onClick={previousImage}
-        className="bg-black text-color-2 hover:bg-[#ecd85d] hover:text-black w-[50px] max-[900px]:absolute max-[900px]:left-0 max-[900px]:h-full max-[900px]:w-[50%] max-[900px]:opacity-0">
-        <AiOutlineArrowLeft className='w-full h-full' />
+        className="bg-black text-color-2 hover:bg-[#ecd85d] hover:text-black w-[50px] max-[900px]:absolute max-[900px]:left-0 max-[900px]:h-full max-[900px]:w-[50%] max-[900px]:opacity-0"
+      >
+        <AiOutlineArrowLeft className="w-full h-full" />
       </button>
 
       <figcaption>
         <img
           src={images[currentImageIndex]}
           alt="imagenProject"
-          className={`${pathname === "/proyectos" ? "h-[800px]" : "h-[495px] w-fit object-contain"}`}
+          className={`${
+            pathname === "/projects"
+              ? "h-[800px]"
+              : "h-[495px] w-fit object-contain"
+          }`}
         />
       </figcaption>
 
       <button
         onClick={nextImage}
-        className="bg-black text-color-2 hover:bg-[#ecd85d] hover:text-black w-[50px] max-[900px]:absolute max-[900px]:right-0 max-[900px]:h-full max-[900px]:opacity-0 max-[900px]:w-[50%] ">
-        <AiOutlineArrowRight className='w-full h-full' />
+        className="bg-black text-color-2 hover:bg-[#ecd85d] hover:text-black w-[50px] max-[900px]:absolute max-[900px]:right-0 max-[900px]:h-full max-[900px]:opacity-0 max-[900px]:w-[50%] "
+      >
+        <AiOutlineArrowRight className="w-full h-full" />
       </button>
 
       <div className="absolute bottom-2 left-0 right-0 flex justify-center">
@@ -45,7 +57,9 @@ const Carousel = ({ images }) => {
           <div
             key={index}
             onClick={() => selectImage(index)}
-            className={`w-2 h-2 mx-1 rounded-full hover:cursor-pointer ${index === currentImageIndex ? 'bg-black' : 'bg-gray-400'}`}
+            className={`w-2 h-2 mx-1 rounded-full hover:cursor-pointer ${
+              index === currentImageIndex ? "bg-black" : "bg-gray-400"
+            }`}
           />
         ))}
       </div>
