47e4a06eb38763173d93e04ce956e030678303e5
Fermin Solaberrieta <fermin.solaberrieta@biggertech.co>
2025-06-15 22:47:32 +0000

    Home Component Updates

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,25 @@
-import About from '../About/About.jsx'
-import Footer from '../Footer/Footer.jsx'
-import Inicio from '../Inicio/Inicio.jsx'
-import Educacion from '../Educacion/Educacion.jsx'
-import ProjectsHighlighted from '../Projects/ProjectsHighlighted.jsx'
-import Technologies from '../Technologies/Technologies.jsx'
-import Contact from '../Contact/Contact.jsx'
-import Arrow from '../Arrow/Arrow.jsx'
+import About from "../About/About.jsx";
+import Footer from "../Footer/Footer.jsx";
+import HomeSection from "./HomeSection.jsx";
+import Education from "../Education/Education.jsx";
+import Experience from "../Experience/Experience.jsx";
+import ProjectsHighlighted from "../Projects/ProjectsHighlighted.jsx";
+import Technologies from "../Technologies/Technologies.jsx";
+import Contact from "../Contact/Contact.jsx";
+import Arrow from "../Arrow/Arrow.jsx";
 
 export default function Home() {
   return (
     <div>
-      <Inicio />
+      <HomeSection />
       <About />
-      <Educacion />
+      <Education />
+      <Experience />
       <Technologies />
       <ProjectsHighlighted />
       <Contact />
       <Arrow />
       <Footer />
     </div>
-  )
+  );
 }
---
+++ src/components/Home/HomeSection.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-scroll";
+
+export default function HomeSection() {
+  return (
+    <section
+      id="home"
+      className="bg-color-4 h-screen font-Montserrat flex flex-col justify-center items-center md:items-start px-10 md:pl-[20%] text-center md:text-left gap-10"
+    >
+      <h1 className="font-extrabold text-4xl color-[#272341]">
+        Hola, mi nombre es{" "}
+        <span className="text-color-2">Solaberrieta, Fermín</span> <br />Y soy
+        FullStack Developer.
+      </h1>
+      <Link
+        to="about"
+        smooth={true}
+        duration={500}
+        className="border-[#ecd85d] hover:bg-[#ecd85d] hover:text-[#181607] border-2 px-2 font-Montserrat text-2xl text-color-2 hover:cursor-pointer"
+      >
+        Sobre mí
+      </Link>
+    </section>
+  );
+}
