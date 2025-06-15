519da333ec9d69020c3afc393b690567abfed6bb
Fermin Solaberrieta <fermin.solaberrieta@biggertech.co>
2025-06-15 22:48:04 +0000

    Arrow Component

--- src/components/Arrow/Arrow.jsx
+++ src/components/Arrow/Arrow.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { FaArrowUp } from 'react-icons/fa';
-import { HiOutlineArrowNarrowUp } from 'react-icons/hi';
-import { Link } from 'react-scroll';
-
+import { useState } from "react";
+import { useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+import { HiOutlineArrowNarrowUp } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 export default function Arrow() {
-
-  const [scroll, setScroll] = useState(false)
+  const [scroll, setScroll] = useState(false);
 
   const changeNav = () => {
     if (window.scrollY >= 1) {
@@ -23,9 +21,14 @@ export default function Arrow() {
 
   return (
     <div className={`fixed left-3 bottom-3 ${scroll ? "show" : "hide"}`}>
-      <Link to="inicio" smooth={true} duration={1000} className='hover:cursor-pointer'>
+      <Link
+        to="home"
+        smooth={true}
+        duration={1000}
+        className="hover:cursor-pointer"
+      >
         <HiOutlineArrowNarrowUp className="w-10 h-10 hover:text-[#ecd85d] hover:cursor-pointer" />
       </Link>
     </div>
-  )
+  );
 }
