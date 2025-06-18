cf1387fbbe4693b8cc53c5c005eef9d705cacb0d
Fermin Solaberrieta <fermin.solaberrieta@biggertech.co>
2025-06-18 12:24:53 +0000

    Update HomeSection component styling with new background color and text colors for improved visibility and aesthetics

--- src/components/Home/HomeSection.jsx
+++ src/components/Home/HomeSection.jsx
@@ -4,18 +4,18 @@ export default function HomeSection() {
   return (
     <section
       id="home"
-      className="bg-color-4 h-screen font-Montserrat flex flex-col justify-center items-center md:items-start px-10 md:pl-[20%] text-center md:text-left gap-10"
+      className="bg-[#0e141b] h-screen font-Montserrat flex flex-col justify-center items-center md:items-start px-10 md:pl-[20%] text-center md:text-left gap-10"
     >
-      <h1 className="font-extrabold text-4xl color-[#272341]">
+      <h1 className="font-extrabold text-4xl text-white">
         Hola, mi nombre es{" "}
-        <span className="text-color-2">Solaberrieta, Fermín</span> <br />Y soy
+        <span className="text-[#ecd85d]">Solaberrieta, Fermín</span> <br />Y soy
         FullStack Developer.
       </h1>
       <Link
         to="about"
         smooth={true}
         duration={500}
-        className="border-[#ecd85d] hover:bg-[#ecd85d] hover:text-[#181607] border-2 px-2 font-Montserrat text-2xl text-color-2 hover:cursor-pointer"
+        className="border-[#ecd85d] hover:bg-[#ecd85d] hover:text-[#0e141b] border-2 px-2 font-Montserrat text-2xl text-[#ecd85d] hover:cursor-pointer"
       >
         Sobre mí
       </Link>
