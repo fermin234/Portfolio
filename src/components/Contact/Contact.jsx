cd666e90608411a8df62d4bde6297bc206b895bc
Fermin Solaberrieta <fermin.solaberrieta@biggertech.co>
2025-06-18 12:24:36 +0000

    Update Contact component styling with new background color and text color for improved visibility and aesthetics

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,11 @@
 export default function Contact() {
   return (
-    <section id="contact" className="w-full py-[4vh] bg-color-6 flex flex-col items-center justify-center font-Montserrat">
-      <h1 className="mb-5 text-[40px] max-[715px]:text-[28px] font-bold text-color-3">CONTACTO</h1>
+    <section id="contact" className="w-full py-[4vh] bg-[#0e141b] flex flex-col items-center justify-center font-Montserrat">
+      <h1 className="mb-5 text-[40px] max-[715px]:text-[28px] font-bold text-white">CONTACTO</h1>
       <a
         rel="noreferrer"
         target="_blank"
-        className="font-extrabold border-[#ecd85d] hover:bg-[#ecd85d] hover:text-[#181607] w-fit border-2 px-2 font-Montserrat text-2xl text-color-2 hover:cursor-pointer"
+        className="font-extrabold border-[#ecd85d] hover:bg-[#ecd85d] hover:text-[#0e141b] w-fit border-2 px-2 font-Montserrat text-2xl text-[#ecd85d] hover:cursor-pointer"
         href="mailto:fermindev@gmail.com"
       >Enviame un email
       </a>
